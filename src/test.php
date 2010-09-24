9273d31f481d322f9960bde083512d8d81c772ca
Anthony Le Mansec <a.lm@removeme.free.fr>
2010-09-24 20:39:28 +0200

    testing current codebase.

--- src/test.php
+++ src/test.php
@@ -7,11 +7,9 @@
 require_once('bootstrap.php');
 
 $viewer = WebAsset_Viewer::instance();
-$viewer->load(new WebAsset("cb2052ae-d161-43e9-b11b-c834217823cd"));
+//$viewer->load(new WebAsset("cb2052ae-d161-43e9-b11b-c834217823cd"));
+$viewer->load(new WebAsset("00000000-0000-0000-0000-000000000000"));
 
-// $rawImage = $viewer->getImageDatas("JPEG");
-// ...
-
-// $cache = WebAsset_Cache::instance();
+$rawImage = $viewer->getImageDatas("JPEG");
 
 ?>
